ad6f3454b208fb05c1c013377c1d32c68639a0c5
Jorge Navarro <jnvazquez@users.noreply.github.com>
2015-06-09 22:03:36 +0000

    Módulo 7 - Ejercicio P2P Obligatorio - Buscador

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,45 @@
-// GET /quizes/question
-exports.question = function(req, res) {
-    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+      function(quiz) {
+        if (quiz) {
+          req.quiz = quiz;
+          next();
+        } else { next(new Error('No existe quizId=' + quizId)); }
+      }
+    ).catch(function(error) {next(error);});
 };
 
-//GET /quizes/answer
-exports.answer = function (req, res) {
-    if (req.query.respuesta === 'Roma') {
-        res.render('quizes/answer', {respuesta: 'Correcto'});
-    } else {
-        res.render('quizes/answer', {respuesta: 'Incorrecto'});
-    }
+// GET /quizes
+exports.index = function(req, res) {
+  var searchStr = req.query.search;
+  if (searchStr !== undefined) {
+    models.Quiz.findAll({ where : ['pregunta like ?', '%' + searchStr.replace(' ', '%') + '%']})
+      .then(function(quizes) {
+        res.render('quizes/index.ejs', { quizes : quizes });
+    })
+  } else {
+    models.Quiz.findAll().then(function(quizes) {
+        res.render('quizes/index.ejs', { quizes : [] });
+    })  
+  }
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  res.render('quizes/show', { quiz : req.quiz});
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer',
+            { quiz : req.quiz, respuesta : resultado});
 };
 
 // GET /author
